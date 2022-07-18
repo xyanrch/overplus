765fd025aa4150530a1dbec0d91db80c072edfa5
xyranch <986814080@qq.com>
2022-07-18 11:56:16 +0800

    handle exception

--- Server/main.cpp
+++ Server/main.cpp
@@ -3,29 +3,36 @@
 //#include<string.h>
 #include "Shared/Log.h"
 #include "Shared/LogFile.h"
+#include <exception>
 #include <string>
 
 int main(int argc, char* argv[])
 {
-    if(argc<2)
-    {
-        std::cerr << "Usage: " << argv[0] << "config path" << std::endl;
-        return 0;
-    }
-    ConfigManage::instance().load_config(argv[1], ConfigManage::Server);
-    LogFile logfile_("server", 10 * 1024 * 1024);
-    logger::set_log_level(ConfigManage::instance().server_cfg.log_level);
-     logger::set_log_destination(Destination::D_FILE);
-    logger::setOutput([&](std::string&& buf) {
-        logfile_.append(std::move(buf));
-    });
-    logger::setFlush([&]() {
-        logfile_.flush();
-    });
+    try {
+        if (argc < 2) {
+            std::cerr << "Usage: " << argv[0] << "config path" << std::endl;
+            return 0;
+        }
+        ConfigManage::instance().load_config(argv[1], ConfigManage::Server);
+        LogFile logfile_("server", 10 * 1024 * 1024);
+        logger::set_log_level(ConfigManage::instance().server_cfg.log_level);
+        logger::set_log_destination(Destination::D_FILE);
+        logger::setOutput([&](std::string&& buf) {
+            logfile_.append(std::move(buf));
+        });
+        logger::setFlush([&]() {
+            logfile_.flush();
+        });
 
-    SslServer server;
-    server.run();
-    NOTICE_LOG<<"Server Stopped,will exit";
+        SslServer server;
+        server.run();
+    }
+
+    catch (const std::exception &e) {
+        ERROR_LOG << "Server got exception,will exit :"<<e.what();
+        
+    }
+    NOTICE_LOG << "Server Stopped,will exit";
 
     return 0;
 }
