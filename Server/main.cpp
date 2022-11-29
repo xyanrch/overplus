eb505765ca0340678ba592066fc77b2aa0bfdf5b
xyanrch <5581943+xyanrch@users.noreply.github.com>
2022-11-29 19:23:16 +0800

    ...

--- Server/main.cpp
+++ Server/main.cpp
@@ -10,39 +10,41 @@
 namespace po = boost::program_options;
 int main(int argc, char* argv[])
 {
-    std::string config_file;
-    po::options_description desc("Allowed options");
-    desc.add_options()                     //
-        ("version,v", "print version string") //
-        ("help,h", "produce help message")    //
-        ("config,c", po::value<std::string>(&config_file)->default_value("server.json"), "config file path");
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-    if (vm.count("help")) {
-        std::cout << desc << std::endl;
-        return 0;
-    }
-    if (vm.count("version")) {
-        std::cout << OVERPLUS_VERSION_STR << std::endl;
-        return 0;
-    }
-    auto& config = ConfigManage::instance();
-    config.load_config(config_file, ConfigManage::Server);
-    std::unique_ptr<LogFile> logfile_;
-
-    logger::set_log_level(config.server_cfg.log_level);
-    if (!config.server_cfg.log_dir.empty()) {
-        logfile_.reset(new LogFile("server", 10 * 1024 * 1024));
-        logger::set_log_destination(Destination::D_FILE);
-        logger::setOutput([&](std::string&& buf) {
-            logfile_->append(std::move(buf));
-        });
-        logger::setFlush([&]() {
-            logfile_->flush();
-        });
-    }
     try {
+        std::string config_file;
+        po::options_description desc("Allowed options");
+        desc.add_options()                        //
+            ("version,v", "print version string") //
+            ("help,h", "produce help message")    //
+            ("config,c", po::value<std::string>(&config_file)->default_value("server.json"), "config file path");
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+        if (vm.count("help")) {
+            std::cout << desc << std::endl;
+            return 0;
+        }
+        if (vm.count("version")) {
+            std::cout << OVERPLUS_VERSION_STR << std::endl;
+            return 0;
+        }
+        auto& config = ConfigManage::instance();
+        config.load_config(config_file, ConfigManage::Server);
+        std::unique_ptr<LogFile> logfile_;
+
+        logger::set_log_level(config.server_cfg.log_level);
+        if (!config.server_cfg.log_dir.empty()) {
+            logfile_.reset(new LogFile("server", 10 * 1024 * 1024));
+            logger::set_log_destination(Destination::D_FILE);
+            logger::setOutput([&](std::string&& buf) {
+                logfile_->append(std::move(buf));
+            });
+            logger::setFlush([&]() {
+                logfile_->flush();
+            });
+        }
+
+        NOTICE_LOG << "overplus " << OVERPLUS_VERSION_STR << " will start..." << std::endl;
         Service server;
         server.run();
     } catch (const std::exception& e) {
