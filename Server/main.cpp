f268cc710f33c8286fc70bbada9bbeedd1438be6
xyanrch <5581943+xyanrch@users.noreply.github.com>
2022-11-29 19:07:14 +0800

    Add arguments parser

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,21 +1,35 @@
 #include "Server/Service.h"
 #include "Shared/ConfigManage.h"
-//#include<string.h>
+// #include<string.h>
 #include "Shared/Log.h"
 #include "Shared/LogFile.h"
+#include "Shared/Version.h"
+#include <boost/program_options.hpp>
 #include <exception>
 #include <string>
-
+namespace po = boost::program_options;
 int main(int argc, char* argv[])
 {
-
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << "config path" << std::endl;
+    std::string config_file;
+    po::options_description desc("Allowed options");
+    desc.add_options()                     //
+        ("version,v", "print version string") //
+        ("help,h", "produce help message")    //
+        ("config,c", po::value<std::string>(&config_file)->default_value("server.json"), "config file path");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
+    if (vm.count("version")) {
+        std::cout << OVERPLUS_VERSION_STR << std::endl;
         return 0;
     }
     auto& config = ConfigManage::instance();
-    config.load_config(argv[1], ConfigManage::Server);
-    std::unique_ptr<LogFile>logfile_;
+    config.load_config(config_file, ConfigManage::Server);
+    std::unique_ptr<LogFile> logfile_;
 
     logger::set_log_level(config.server_cfg.log_level);
     if (!config.server_cfg.log_dir.empty()) {
